89c0cb92feb5b08ca09c7dd48870127d353b2939
Marc Meijer <marmeijer@uni-osnabrueck.de>
2023-08-12 16:02:01 +0200

    fixed conversions

--- to_wav.py
+++ to_wav.py
@@ -7,10 +7,13 @@ from typing import List
 INPUT_DIR = "dataset_raw"
 OUTPUT_DIR = "dataset_converted"
 
+FIRST_MIN = 60000
+
 m_files: List[str] = os.listdir(INPUT_DIR)
 
 for f in m_files:
     song: AudioSegment = AudioSegment.from_mp3(os.path.join(INPUT_DIR, f))
-    song.export(os.path.join(OUTPUT_DIR, os.path.splitext(f)[0][0:-1] + ".wav"), format="wav")
+    sliced_ten_secs = song[FIRST_MIN:FIRST_MIN+10000]
+    sliced_ten_secs.export(os.path.join(OUTPUT_DIR, os.path.splitext(f)[0] + ".wav"), format="wav")
 
 
