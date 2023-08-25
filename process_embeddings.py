5e68cb7ee5100e267d787d677dcf32c285c01752
Marc Meijer <marmeijer@uni-osnabrueck.de>
2023-08-25 15:03:58 +0200

    return numpy array instead of regular list

--- process_embeddings.py
+++ process_embeddings.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 import requests
+import numpy as np
+import numpy.typing as npt
 
-def get_embedding(filepath: str, embedding_gen_url: str="http://127.0.0.1:5000/model/predict"):
+def get_embedding(filepath: str, embedding_gen_endp: str="http://127.0.0.1:5000/model/predict") -> npt.NDArray[np.float32]:
+    """get embeddings as a numpy ndarray out of floats for a music file
+    from a server endpoint specified through embedding_gen_endp
+    """
     files = {
         "audio": (filepath, open(filepath, "rb"), "audio/x-wav")
     }
-    response = requests.post(embedding_gen_url, files=files)
-    return response.json()["embedding"]
+    response = requests.post(embedding_gen_endp, files=files)
+    return np.array(response.json()["embedding"])
 
 if __name__ == "__main__":
-    print(get_embedding("dataset_converted/Beast - Vicetone Vs. Nico Vega.wav")) 
+    embd = get_embedding("dataset_converted/Beast - Vicetone Vs. Nico Vega.wav")
+    print(embd.flatten())
