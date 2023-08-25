a4732cd02885be69a6ca07693946d879a9507c94
carends <carends@uni-osnabrueck.de>
2023-08-25 14:55:41 +0200

    auslagerung in methoden

--- to_wav.py
+++ to_wav.py
@@ -7,92 +7,66 @@ import argparse
 import sys
 
 
-parser = argparse.ArgumentParser(description="Formats a music file of various formats into a formatted wav music file")
+def is_directory(arg:str):
+    if not os.path.isdir(arg):
+        raise argparse.ArgumentTypeError("The givin argument was not an valid Path!")
+    return arg
 
+def parse_arguments():
 
-# MIT FLAGS::
+    parser = argparse.ArgumentParser(description="Formats a music file of various formats into a formatted wav music file")
+    group = parser.add_mutually_exclusive_group(required=True)
 
-#parser.add_argument('-f', default=None, help="", required=True)
-#args = parser.parse_args()
-#print(args.f)
-#parser.print_help()
+    group.add_argument('-d', '--directory', help="takes a directory and converts everything to an wav file")
+    group.add_argument('-f', '--file', help="takes an file and on base of this file will perferm an recommendation")
 
-##
+  
 
-
-# MIT PARSER GRUPPEN
-
-group = parser.add_mutually_exclusive_group(required=True)
-
-group.add_argument('-d', '--directory', help="takes a directory and converts everything to an wav file")
-group.add_argument('-f', '--file', help="takes an file and on base of this file will perferm an recommendation")
-
-#parser.add_argument('-df', '--destinationfile', help="destination, where the file should be safed", default="requestet_song_converted")
-#parser.add_argument('-dd', '--destinationdirectory', help="destination, where the datasat should be safed", default="dataset_converted")
-
-#BEIDE ZUSAMMENGEFASST ZU EINEN:    
-
-parser.add_argument('-df', '--destinationfolder', help="takes an filepath, where the convertet file will be stored",
+    parser.add_argument('-df', '--destinationfolder', type=is_directory, help="takes an filepath, where the convertet file will be stored",
                     default=None)
 
 
-args = parser.parse_args()
+    return parser.parse_args()
 
-##
-
-
-FIRST_MIN = 60000
-
-
-if args.directory:
-    print(args.directory)
-    if args.destinationfolder == None:
-        args.destinationfolder = "dataset_converted"
-    OUTPUT_DIR = os.path.join(os.path.dirname(__file__), args.destinationfolder)
-    INPUT_DIR = os.path.join(os.path.dirname(__file__), args.directory) 
-    m_files: List[str] = os.listdir(INPUT_DIR)
-
-    for f in m_files:
-        song: AudioSegment = AudioSegment.from_mp3(os.path.join(INPUT_DIR, f))
-        sliced_ten_secs = song[FIRST_MIN:FIRST_MIN+10000]
-        sliced_ten_secs.export(os.path.join(OUTPUT_DIR, os.path.splitext(f)[0] + ".wav"), format="wav")
+def converter(des:str, imp:str, is_dir:bool):
+    OUTPUT_DIR = os.path.join(os.path.dirname(__file__), des)
+    INPUT_DIR = os.path.join(os.path.dirname(__file__), imp) 
+    FIRST_MIN = 60000
     
+    if is_dir == True:
+        m_files: List[str] = os.listdir(INPUT_DIR)
+        for f in m_files:
+            song: AudioSegment = AudioSegment.from_mp3(os.path.join(INPUT_DIR, f))
+            sliced_ten_secs = song[FIRST_MIN:FIRST_MIN+10000]
+            sliced_ten_secs.export(os.path.join(OUTPUT_DIR, os.path.splitext(f)[0] + ".wav"), format="wav")
+            
+    elif is_dir == False:
+        dir = os.listdir(os.path.join(os.path.dirname(__file__), des))
+        if len(dir) == 0: 
+            song: AudioSegment = AudioSegment.from_mp3(os.path.join(INPUT_DIR))
+            sliced_ten_secs = song[FIRST_MIN:FIRST_MIN+10000]
+            sliced_ten_secs.export(os.path.join(OUTPUT_DIR, os.path.splitext(imp)[0] + ".wav"), format="wav")
+        else: 
+            raise ValueError("Destination Folder: " + des + " is not empty")
 
-elif args.file:
-    print(args.file)
-    if args.destinationfolder == None:
-        args.destinationfolder = "requestet_song_converted"
-    dir = os.listdir(os.path.join(os.path.dirname(__file__), args.destinationfolder))
+
+
+if __name__ == "__main__":
+
+    args = parse_arguments()
+
+
+    if args.directory:
+        if args.destinationfolder == None:
+            if not os.path.isdir("dataset_converted"):
+                os.makedirs("dataset_converted")
+            args.destinationfolder = "dataset_converted"
+        converter(args.destinationfolder, args.directory, True)
+    elif args.file:
+        if args.destinationfolder == None:
+            if not os.path.isdir("requestet_song_converted"):
+                os.makedirs("requestet_song_converted")
+            args.destinationfolder = "requestet_song_converted"
+        converter(args.destinationfolder, args.file, False)
+
     
-    if len(dir) == 0: 
-        print("Empty directory") 
-        OUTPUT_DIR_request = os.path.join(os.path.dirname(__file__), args.destinationfolder)
-        INPUT_DIR_request = os.path.join(os.path.dirname(__file__), args.file)
-        requestet_song: AudioSegment = AudioSegment.from_mp3(os.path.join(INPUT_DIR_request))
-
-        
-
-        sliced_ten_secs1 = requestet_song[FIRST_MIN:FIRST_MIN+10000]
-        sliced_ten_secs1.export(os.path.join(OUTPUT_DIR_request, os.path.splitext(args.file)[0] + ".wav"), format="wav")
-        #print(mediainfo("requestet_song_converted\Bück_Dich_hoch_-_Deichkind.wav"))
-    else: 
-        print("Not empty directory")
-        raise ValueError("Destination Folder: " + args.destinationfolder + " is not empty")
-        
-    
-    
-
-# if args.f != None:
- #   INPUT_DIR_request = os.path.join(os.path.dirname(__file__), args.f)
-  #  requestet_song: AudioSegment = AudioSegment.from_mp3(os.path.join(INPUT_DIR_request))
-   # sliced_ten_secs1 = requestet_song[FIRST_MIN:FIRST_MIN+10000]
-    #sliced_ten_secs1.export(os.path.join(OUTPUT_DIR_request, os.path.splitext(args.f)[0] + ".wav"), format="wav")
-
-
-#m_files: List[str] = os.listdir(INPUT_DIR)
-
-#for f in m_files:
- #   song: AudioSegment = AudioSegment.from_mp3(os.path.join(INPUT_DIR, f))
-  #  sliced_ten_secs = song[FIRST_MIN:FIRST_MIN+10000]
-   # sliced_ten_secs.export(os.path.join(OUTPUT_DIR, os.path.splitext(f)[0] + ".wav"), format="wav")
-
