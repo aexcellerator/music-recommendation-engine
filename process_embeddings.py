5282bfb80d4e8092c5de136af2dc27ba2bc7d508
Marc Meijer <marmeijer@uni-osnabrueck.de>
2023-08-29 18:31:24 +0200

    added error handling to process_embeddings

--- process_embeddings.py
+++ process_embeddings.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+import sys
 import requests
+from requests.exceptions import ConnectionError
 import numpy as np
 import numpy.typing as npt
 
@@ -10,7 +12,17 @@ def get_embedding(filepath: str, embedding_gen_endp: str="http://127.0.0.1:5000/
     files = {
         "audio": (filepath, open(filepath, "rb"), "audio/x-wav")
     }
-    response = requests.post(embedding_gen_endp, files=files)
+    
+    try:
+        response = requests.post(embedding_gen_endp, files=files)
+    except ConnectionError:
+        print(f'Could not connect to the server endpoint "{embedding_gen_endp}". Is the MAX Audio Embedding Generator correctly setup?')
+        sys.exit(1)
+
+    if response.status_code != 200:
+        print(f'Something went wrong on sending a post request to endpoint "{embedding_gen_endp}". Aborting...')
+        sys.exit(1)
+
     return np.array(response.json()["embedding"])
 
 if __name__ == "__main__":
