9eb16f5f2dc063f78efdd15a332763406874d273
Zamenis-longissimus <132847357+Zamenis-longissimus@users.noreply.github.com>
2023-08-29 20:31:19 +0000

    grammatical overhaul

--- to_wav.py
+++ to_wav.py
@@ -8,31 +8,31 @@ import argparse
 import sys
 
 
-def is_directory(arg:str):
+def is_directory(arg: str):
     if not os.path.isdir(arg):
-        raise argparse.ArgumentTypeError("The givin argument was not an valid Path!")
+        raise argparse.ArgumentTypeError("The given argument was not a valid Path!")
     return arg
 
 def parse_arguments():
 
-    parser = argparse.ArgumentParser(description="Formats a music file of various formats into a formatted wav music file")
+    parser = argparse.ArgumentParser(description="formats a music file of various formats into a formatted wav music file")
     group = parser.add_mutually_exclusive_group(required=True)
 
-    group.add_argument('-d', '--directory', help="takes a directory and converts everything to an wav file")
-    group.add_argument('-f', '--file', help="takes an file and on base of this file will perferm an recommendation")
+    group.add_argument('-d', '--directory', help="takes a directory and converts everything to a wav file")
+    group.add_argument('-f', '--file', help="file on which basis a recommendation will be performed")
 
-    parser.add_argument('-df', '--destinationfolder', type=is_directory, help="takes an filepath, where the convertet file will be stored",
+    parser.add_argument('-df', '--destinationfolder', type=is_directory, help="filepath where the converted file will be stored",
                     default=None)
     
-    parser.add_argument('-t', '--starttime', type=int, help="Time at which the clip starts in ms",
+    parser.add_argument('-t', '--starttime', type=int, help="starttime of the song(s) as of when the embedding will be created in ms",
                     default=60000)
-    parser.add_argument('-l', '--length', type=int,  help="Value how long the clip should be in ms",
+    parser.add_argument('-l', '--length', type=int,  help="length of the song excerpt, on which basis the embedding is calculated in ms",
                     default=10000)
     
     return parser.parse_args()
 
 
-def converter(inpd:str, inpf:str, des:str, starttime: int, length: int):
+def converter(inpd: str, inpf: str, des: str, starttime: int, length: int):
 
     if inpf == None:
         if des == None:
@@ -56,7 +56,7 @@ def converter(inpd:str, inpf:str, des:str, starttime: int, length: int):
                 song_clip.export(os.path.join(output_dir, os.path.splitext(f)[0] + ".wav"), format="wav")
                 
         else:
-            print("The folder '" + input_dir + "' is empty!")
+            print("The directory '" + input_dir + "' is empty!")
             sys.exit()
 
         ret = [os.path.join(des, x) for x in os.listdir(output_dir)]
@@ -75,7 +75,7 @@ def converter(inpd:str, inpf:str, des:str, starttime: int, length: int):
             song_clip = song[starttime:starttime+length]
             song_clip.export(os.path.join(output_dir, os.path.splitext(inpf)[0] + ".wav"), format="wav")
         else: 
-            raise ValueError("Destination Folder: '" + des + "' is not empty")
+            raise ValueError("Destination directory: '" + des + "' is not empty")
         
         return os.path.join(des, os.listdir(output_dir)[0])
     
@@ -84,4 +84,3 @@ if __name__ == "__main__":
 
     args = parse_arguments()
     print(converter(args.directory, args.file, args.destinationfolder, args.starttime, args.length))
-    
