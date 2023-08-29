105e73faa7d8cb26c980c581d3b8e1ce02df3489
carends <carends@uni-osnabrueck.de>
2023-08-29 18:37:50 +0200

    kurze main

--- to_wav.py
+++ to_wav.py
@@ -24,68 +24,71 @@ def parse_arguments():
     parser.add_argument('-df', '--destinationfolder', type=is_directory, help="takes an filepath, where the convertet file will be stored",
                     default=None)
     
-    parser.add_argument('-t', '--starttime', help="Time at which the clip starts in ms",
+    parser.add_argument('-t', '--starttime', type=int, help="Time at which the clip starts in ms",
                     default=60000)
-    parser.add_argument('-l', '--length', help="Value how long the clip should be in ms",
+    parser.add_argument('-l', '--length', type=int,  help="Value how long the clip should be in ms",
                     default=10000)
     
     return parser.parse_args()
 
-def converter(des:str, imp:str, is_dir:bool, starttime: int, length: int):
-    OUTPUT_DIR = os.path.join(os.path.dirname(__file__), des)
-    INPUT_DIR = os.path.join(os.path.dirname(__file__), imp) 
-    
-    if is_dir == True:
-        m_files: List[str] = os.listdir(INPUT_DIR)
+
+def converter(impd:str, impf:str, des:str, starttime: int, length: int):
+
+    ret =[]
+    if impf == None:
+        ret.append("directory")
+        ret.append(impd)
+        if des == None:
+            if not os.path.isdir("dataset_converted"):
+                os.makedirs("dataset_converted")
+            des = "dataset_converted"
+        ret.append(des)
+        input_dir = os.path.join(os.path.dirname(__file__), impd)
+        output_dir = os.path.join(os.path.dirname(__file__), des)
+
+        m_files: List[str] = os.listdir(input_dir)
         if len(m_files) != 0:
             for f in m_files:
-                s = os.path.splitext(f)[1]
+                s = os.path.splitext(f)[-1]
                 try:
-                    song: AudioSegment = AudioSegment.from_file(os.path.join(INPUT_DIR, f), s[1:])
+                    song: AudioSegment = AudioSegment.from_file(os.path.join(input_dir, f), s[1:])
                 except dubex.CouldntDecodeError:
                     print("The format '" + s + "' is not supported")
                     sys.exit()
+                
                 sliced_ten_secs = song[starttime:starttime+length]
-                print(os.path.splitext(f)[1])
-                sliced_ten_secs.export(os.path.join(OUTPUT_DIR, os.path.splitext(f)[0] + ".wav"), format="wav")
+                sliced_ten_secs.export(os.path.join(output_dir, os.path.splitext(f)[0] + ".wav"), format="wav")
+                
         else:
-            print("The folder '" + INPUT_DIR + "' is empty!")
+            print("The folder '" + input_dir + "' is empty!")
+            sys.exit()
 
-        return m_files            
-    elif is_dir == False:
+        ret = [os.path.join(des, x) for x in os.listdir(output_dir)]
+        return ret
+    else:
+        ret.append("file") 
+        ret.append(impf)
+        if des == None:
+            if not os.path.isdir("requestet_song_converted"):
+                os.makedirs("requestet_song_converted")
+            des = "requestet_song_converted"
+        ret.append(des)
+        input_dir = os.path.join(os.path.dirname(__file__), impf)
+        output_dir = os.path.join(os.path.dirname(__file__), des)
         dir = os.listdir(os.path.join(os.path.dirname(__file__), des))
         if len(dir) == 0: 
-            song: AudioSegment = AudioSegment.from_mp3(os.path.join(INPUT_DIR))
+            s = os.path.splitext(input_dir)[-1]
+            song: AudioSegment = AudioSegment.from_file(os.path.join(input_dir), s[1:])
             sliced_ten_secs = song[starttime:starttime+length]
-            sliced_ten_secs.export(os.path.join(OUTPUT_DIR, os.path.splitext(imp)[0] + ".wav"), format="wav")
+            sliced_ten_secs.export(os.path.join(output_dir, os.path.splitext(impf)[0] + ".wav"), format="wav")
         else: 
             raise ValueError("Destination Folder: '" + des + "' is not empty")
-    
+        
+        return os.path.join(des, os.listdir(output_dir)[0])
     
 
 if __name__ == "__main__":
 
     args = parse_arguments()
-    ret =[]
-    if args.directory:
-        ret.append("directory")
-        ret.append(args.directory)
-        if args.destinationfolder == None:
-            if not os.path.isdir("dataset_converted"):
-                os.makedirs("dataset_converted")
-            args.destinationfolder = "dataset_converted"
-        ret.append(args.destinationfolder)
-        converter(args.destinationfolder, args.directory, True, args.starttime, args.length)
-    elif args.file:
-        ret.append("file")
-        ret.append(args.file)
-        if args.destinationfolder == None:
-            if not os.path.isdir("requestet_song_converted"):
-                os.makedirs("requestet_song_converted")
-            args.destinationfolder = "requestet_song_converted"
-        ret.append(args.destinationfolder)
-        converter(args.destinationfolder, args.file, False, args.starttime, args.length)
-
-    
-
+    print(converter(args.directory, args.file, args.destinationfolder, args.starttime, args.length))
     
